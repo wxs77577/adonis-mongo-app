6e5223998783786bcc1c5e81bfdf154c1f208948
xuesong <wu-xuesong@qq.com>
2018-07-08 01:30:10 +0800

    add test code

--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,18 @@ const Route = use('Route')
 Route.get('/', ({ request }) => {
   return { greeting: 'Hello world in JSON' }
 })
+
+// Uncomment the following code to test with mongodb
+
+// Route.get('/test', async () => {
+//   const User = use('App/Models/User')
+//   await User.findOrCreate({
+//     name: 'adonis-mongo-app'
+//   }, {
+//     name: 'adonis-mongo-app',
+//     github: 'https://github.com/wxs77577/adonis-mongo-app',
+//     cmd: 'adonis new api-server --blueprint wxs77577/adonis-mongo-app',
+//     'cmd-cnpm': 'adonis new api-server --blueprint wxs77577/adonis-mongo-app --cnpm'
+//   })
+//   return await User.query().sort('-_id').paginate(1)
+// })
